5af0c9517a01714a7efbf13425f059097171eb01
hailuo <550847434@qq.com>
2019-09-08 21:02:25 +0800

    get all ready

--- app/src/main/java/com/progress_android/MonthlyPlan/MonthlyPlanActivity.java
+++ app/src/main/java/com/progress_android/MonthlyPlan/MonthlyPlanActivity.java
@@ -11,6 +11,6 @@ public class MonthlyPlanActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_daily_plan);
+        setContentView(R.layout.activity_monthly_plan);
     }
 }
