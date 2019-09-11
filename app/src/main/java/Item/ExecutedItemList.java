bed1a9075d16b8e32fd45f050b83193a52187183
Yamaa <958457134@qq.com>
2019-09-11 18:30:09 +0800

    添加了统计界面中的时间分配界面

---
+++ app/src/main/java/Item/ExecutedItemList.java
@@ -0,0 +1,28 @@
+package Item;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExecutedItemList {
+    List<ExecutedItem> executedItemList = new ArrayList<>();
+    ExecutedItem currentItem = null;
+
+    public ExecutedItemList(){
+
+    }
+
+    //该函数在为一已有项添加开始与结束时间时需要用到
+    public void addOrderPoint(ExecutedItem executedItem){
+        if(currentItem!=null) {
+            currentItem.addNext(executedItem);
+        }
+        currentItem = executedItem;
+    }
+
+    //添加新项调用
+    public void addExecutedItem(ExecutedItem executedItem){
+        executedItemList.add(executedItem);
+        addOrderPoint(executedItem);
+        currentItem = executedItem;
+    }
+}
