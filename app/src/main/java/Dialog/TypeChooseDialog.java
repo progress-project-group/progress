f6d5bca4e2510e8dae5f1b89ee09e36cf228c2a7
Yamaa <958457134@qq.com>
2019-09-25 14:27:04 +0800

    事件明细界面完成，添加了一周统计界面，时间轴统计界面效果不理想考虑删除该部分。至此统计界面完成

--- app/src/main/java/Dialog/TypeChooseDialog.java
+++ app/src/main/java/Dialog/TypeChooseDialog.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
+import com.progress_android.DailySummaryActivity;
 import com.progress_android.R;
 
 import java.util.ArrayList;
